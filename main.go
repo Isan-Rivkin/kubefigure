3f8f93ff18ca420cdd9158d83eac1c0cdc58a27d
Isan-Rivkin <isanrivkin@gmail.com>
2021-12-17 15:15:08 +0200

    added terraform stuff

--- main.go
+++ main.go
@@ -33,7 +33,8 @@ func test1() {
 }
 func test2() {
 	bucket := "similarweb-terraform-remote-states"
-	key := "private/terraform/route53-zones/int-similarweb-io/terraform.tfstate"
+	//key := "private/terraform/route53-zones/int-similarweb-io/terraform.tfstate"
+	key := "private/terraform/nomad_agent/us-east-1_production_datacollection/terraform.tfstate"
 	region := "us-east-1"
 
 	connector := terraform.NewDefaultS3RemoteStateConnector(bucket, key, region)
@@ -63,6 +64,29 @@ func test2() {
 		log.Info("key " + key)
 		log.Info(val.Value.Type().GoString())
 		log.Info(val.Value)
+	}
+
+	log.Info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!!!!")
+	for _, m := range state.File.State.Modules {
+		log.Info(m.Addr)
+		for k, r := range m.Resources {
+			log.WithFields(log.Fields{
+				"resource":      k,
+				"resource_addr": r.Addr,
+			}).Info("A resource!")
+
+			for i, d := range r.Instances {
+				log.WithFields(log.Fields{
+					"instances": i,
+					"data":      string(d.Current.AttrsJSON),
+				}).Info("An instances!")
+				for _, dep := range d.Current.Dependencies {
+					log.WithFields(log.Fields{
+						"addr": dep.String(),
+					}).Info("Depends on!")
+				}
+			}
+		}
 	}
 }
 
