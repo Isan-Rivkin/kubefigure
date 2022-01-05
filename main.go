12fed73f9b9ece86367bbf454d44ce7a9b712ece
Isan-Rivkin <isanrivkin@gmail.com>
2022-01-05 22:46:14 +0200

    align

--- main.go
+++ main.go
@@ -1,33 +1,43 @@
 package main
 
 import (
-	"github.com/isan-rivkin/kubefigure/sources/awsclient"
 	"github.com/isan-rivkin/kubefigure/sources/terraform"
 	log "github.com/sirupsen/logrus"
 )
 
 func test1() {
 	bucket := "similarweb-terraform-remote-states"
-	key := "private/terraform/route53-zones/int-similarweb-io/terraform.tfstate"
+	//key := "private/terraform/route53-zones/int-similarweb-io/terraform.tfstate"
+	key := "private/terraform/nomad_agent/us-east-1_production_datacollection/terraform.tfstate"
 	region := "us-east-1"
-	s3Api := awsclient.NewS3API(awsclient.CreateNewSession("", "", "", region))
-	connector := terraform.NewS3RemoteStateConnector(s3Api, bucket, key, region)
 
-	file, err := connector.DownloadAsStatefile()
+	connector := terraform.NewDefaultS3RemoteStateConnector(bucket, key, region)
+
+	tfClient := terraform.NewTerraformClient()
+	state, err := tfClient.GetState(connector)
 
 	if err != nil {
-		log.WithError(err).Error("failed getting s3")
+		log.Error("failed getting state")
+		panic(err)
 	}
 
-	outputs, err := terraform.GetOutputsFromStateFile(file)
+	outputs, err := state.OutputsStatus()
 
 	if err != nil {
-		log.WithError(err).Error("failed getting s3")
-		return
+		log.Error("failed getting state outputs")
+		panic(err)
 	}
 
 	for _, o := range outputs {
 		log.Info(o.Key + " : " + o.Value)
+	}
+
+	vals := terraform.GetOutputsValuesFromStateFile(state.File)
+	log.Info("@@@@@@@@@@")
+	for key, val := range vals {
+		log.Info("key " + key)
+		log.Info(val.Value.Type().GoString())
+		log.Info(val.Value)
 	}
 
 }
@@ -67,7 +77,8 @@ func test2() {
 	}
 
 	log.Info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!!!!")
-	for _, m := range state.File.State.Modules {
+	for n, m := range state.File.State.Modules {
+		log.Info(n)
 		log.Info(m.Addr)
 		for k, r := range m.Resources {
 			log.WithFields(log.Fields{
@@ -90,6 +101,102 @@ func test2() {
 	}
 }
 
+func test3() {
+	bucket := "similarweb-terraform-remote-states"
+	//key := "private/terraform/route53-zones/int-similarweb-io/terraform.tfstate"
+	key := "private/terraform/nomad_agent/us-east-1_production_datacollection/terraform.tfstate"
+	region := "us-east-1"
+
+	connector := terraform.NewDefaultS3RemoteStateConnector(bucket, key, region)
+
+	tfClient := terraform.NewTerraformClient()
+	state, err := tfClient.GetState(connector)
+
+	if err != nil {
+		log.Error("failed getting state")
+		panic(err)
+	}
+
+	outputs, err := state.OutputsStatus()
+
+	if err != nil {
+		log.Error("failed getting state outputs")
+		panic(err)
+	}
+
+	for _, o := range outputs {
+		log.Info(o.Key + " : " + o.Value)
+	}
+
+	vals := terraform.GetOutputsValuesFromStateFile(state.File)
+	log.Info("@@@@@@@@@@")
+	for key, val := range vals {
+		log.Info("key " + key)
+		log.Info(val.Value.Type().GoString())
+		log.Info(val.Value)
+	}
+
+	log.Info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@!!!!")
+	state.File.State.RootModule()
+	for n, m := range state.File.State.Modules {
+		log.Info(n)
+		log.Info(m.Addr)
+		if n == "" {
+			log.Info("EMPRTY ADDD!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+			for resName := range m.Resources {
+				log.Info(resName)
+			}
+			log.Info("END EMPRTY ADDD!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+		}
+		// for k, r := range m.Resources {
+		// 	log.WithFields(log.Fields{
+		// 		"resource":      k,
+		// 		"resource_addr": r.Addr,
+		// 	}).Info("A resource!")
+
+		// 	for i, d := range r.Instances {
+		// 		log.WithFields(log.Fields{
+		// 			"instances": i,
+		// 			"data":      string(d.Current.AttrsJSON),
+		// 		}).Info("An instances!")
+		// 		for _, dep := range d.Current.Dependencies {
+		// 			log.WithFields(log.Fields{
+		// 				"addr": dep.String(),
+		// 			}).Info("Depends on!")
+		// 		}
+		// 	}
+		// }
+	}
+}
+
+func test4() {
+	bucket := "similarweb-terraform-remote-states"
+	//key := "private/terraform/route53-zones/int-similarweb-io/terraform.tfstate"
+	key := "private/terraform/nomad_agent/us-east-1_production_datacollection/terraform.tfstate"
+	region := "us-east-1"
+
+	connector := terraform.NewDefaultS3RemoteStateConnector(bucket, key, region)
+
+	tfClient := terraform.NewTerraformClient()
+	state, err := tfClient.GetState(connector)
+
+	if err != nil {
+		log.Error("failed getting state")
+		panic(err)
+	}
+
+	for _, addr := range state.ListModulesAddrs() {
+		log.WithField("address", addr).Info("a module!")
+	}
+
+	for _, addr := range state.ListAllResources() {
+		log.WithField("address", addr).Info("a resources!")
+	}
+
+}
 func main() {
-	test2()
+	//test1()
+	//test2()
+	test3()
+	//test4()
 }
--- sources/terraform/state.go
+++ sources/terraform/state.go
@@ -21,6 +21,36 @@ func NewStateFromFile(File *statefile.File) *TFState {
 	}
 }
 
+// returns all addresses of modules including full addresses of internal nested modules of other modules
+// list include empty string as well which is the root module. i.e resources inside the state with no actual module
+// module.A
+// module.B
+// module.A.module.C
+// module.B.module.D
+func (s *TFState) ListModulesAddrs() []string {
+	var addrs []string
+	for addr := range s.File.State.Modules {
+		addrs = append(addrs, addr)
+	}
+	return addrs
+}
+
+// example
+// data.terraform_remote_state.my_important_state\
+// aws_security_group_rule.my_sg
+// module.mod-name.module.internal-mod-name.datadog_monitor.my_dd_alert
+func (s *TFState) ListAllResources() []string {
+	var resources []string
+	for _, module := range s.File.State.Modules {
+		for _, r := range module.Resources {
+			resources = append(resources, r.Addr.String())
+		}
+	}
+	return resources
+}
+
+//func (s *TFState) GetAllResourceInstances()
+
 func (s *TFState) OutputsStatus() ([]*OutputStatus, error) {
 	outputs, err := GetOutputsFromStateFile(s.File)
 
